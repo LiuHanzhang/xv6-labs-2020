4a631e0b50ddf4c2a96b38a553ff2c21898d8faa
liuhanzhang <liuhanzhang@debian.localdomain>
2021-03-03 18:43:55 +0800

    print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+
+  for(int i =0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      // this PTE is valid.
+      pagetable_t child = (pagetable_t)PTE2PA(pte);
+      printf("..%d: pte %p pa %p\n", i, pte, child);
+      for(int j =0; j < 512; j++){
+        pte_t pte2 = child[j];
+        if(pte2 & PTE_V){
+          pagetable_t child2 = (pagetable_t)PTE2PA(pte2);
+          printf(".. ..%d: pte %p pa %p\n", j, pte2, child2);
+          for(int k =0; k < 512; k++){
+            pte_t pte3 = child2[k];
+            if(pte3 & PTE_V){
+              uint64 pa = PTE2PA(pte3);
+              printf(".. .. ..%d: pte %p pa %p\n", k, pte3, pa);
+            }
+          }
+        }
+      }
+    }
+  }
+}
